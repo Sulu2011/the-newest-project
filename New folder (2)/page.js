ce9b9b84a9b398e09e6672621aa966afdba76c93
Sulu2011 <126379528+Sulu2011@users.noreply.github.com>
2025-03-16 15:03:12 +0400

    Add files via upload

---
+++ New folder (2)/page.js
@@ -0,0 +1,10 @@
+
+let name=prompt("Mənim adım nədir?");
+let age=prompt("Mənim neçə yaşım var?");
+let kod=prompt("Gizli kodu nədir?(Bunu məndən soruşmalısan :D)");
+if (name=="Süleyman" && age=="13"&& kod=="2011") {
+  alert("Məni tanıyırmışsan!Buyur saytımın linki:")
+}
+else{
+  alert("Təəsüfki həqiqətən tanımırsan");
+}
